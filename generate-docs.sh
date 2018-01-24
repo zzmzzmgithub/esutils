b68d339da2721645dd1ce2878a074616628575e2
Cosmin Marginean <cos.marginean@gmail.com>
2018-01-24 18:44:19 +0000

    Java docs

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,1 +1,17 @@
 #!/usr/bin/env bash
+
+
+rm -rf temp
+mkdir temp
+cd temp
+git clone https://github.com/wirktop/esutils
+cd esutils
+version=`cat pom.xml | grep -m 1 '<version>' | sed 's/.*<version>//; s/<\/version>.*//'`
+mvn javadoc:javadoc
+cd ../../
+
+rm -rf $version
+mkdir $version
+cp -R temp/esutils/target/site/apidocs/* $version
+
+rm -rf temp
