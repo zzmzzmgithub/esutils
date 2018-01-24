8f777afab6f93c05ab01967da9964b7badeb3650
Cosmin Marginean <cos.marginean@gmail.com>
2018-01-24 19:08:00 +0000

    Cleanup

--- generate-docs.sh
+++ generate-docs.sh
@@ -7,11 +7,15 @@ cd temp
 git clone https://github.com/wirktop/esutils
 cd esutils
 version=`cat pom.xml | grep -m 1 '<version>' | sed 's/.*<version>//; s/<\/version>.*//'`
-mvn javadoc:javadoc
+mvn clean test javadoc:javadoc
 cd ../../
 
 rm -rf $version
 mkdir $version
-cp -R temp/esutils/target/site/apidocs/* $version
+
+mkdir $version/apidocs
+cp -R temp/esutils/target/site/apidocs $version
+mkdir $version/code-coverage
+cp -R temp/esutils/etc/code-coverage/jacoco-ut/* $version/code-coverage
 
 rm -rf temp
